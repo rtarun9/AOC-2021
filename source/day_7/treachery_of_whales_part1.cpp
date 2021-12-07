6e129cec71f81fd5fe4bf30f77c033150b3a931b
rtarun9 <TarunRamaswamy@outlook.com>
2021-12-07 11:47:44 +0530

    Day 7 solutions

---
+++ source/day_7/treachery_of_whales_part1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+
+int main()
+{
+    std::vector<int> positions = 
+    {
+        #include "input.txt"
+    };
+
+    uint64_t minimum_cost = std::accumulate(positions.begin(), positions.end(), 0);
+
+    for (int i = 0; i < positions.size(); i++)
+    {
+        uint64_t cost_for_index = 0;
+        for (int j = 0; j < positions.size(); j++)
+        {
+            cost_for_index += std::abs(positions[j] - i); 
+        }
+    
+        minimum_cost = std::min(cost_for_index, minimum_cost);
+    }
+
+    std::cout << minimum_cost;
+}
---
+++ source/day_7/treachery_of_whales_part2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+
+int main()
+{
+    std::vector<int> positions = 
+    {
+        #include "input.txt"
+    };
+
+    uint64_t minimum_cost = 0;
+    for (std::size_t i = 0; i < positions.size(); i++)
+    {
+        minimum_cost += positions[i] * (positions[i] + 1) / 2;
+    }
+
+    for (std::size_t i = 1; i < positions.size(); i++)
+    {
+        uint64_t cost_for_index = 0;
+        uint64_t positions_to_move = 0;
+        for (std::size_t j = 0; j < positions.size(); j++)
+        {
+            positions_to_move =  std::abs(positions[(int)j] - (int)i); 
+            cost_for_index += positions_to_move * (positions_to_move + 1) / 2;
+        }
+
+        minimum_cost = std::min(cost_for_index, minimum_cost);
+    }
+
+    std::cout << minimum_cost;
+}
